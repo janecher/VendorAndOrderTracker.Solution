8527a606c86fb6da0d6d17ec362e4e33fed101c4
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-24 10:53:27 -0700

    add properties and constructor

---
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public int Id {get;}
+    public List<Order> Orders {get; set;}
+    private List<Vendor> _vendors = new List<Vendor>();
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      Orders = new List<Order>();
+      _vendors.Add(this);
+      Id = _vendors.Count;
+    }
+
+  }
+}
