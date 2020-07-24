5c9e1510d26ba0348e991962e0caf01d4334736e
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-24 10:33:33 -0700

    add tests for GetAll method and check Id, check Id failed before I add IDisposable interface, all test passed after implementing Dispose method by ClearAll and GetAll methods in Order

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -6,14 +6,47 @@ using System;
 namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class OrderTest
+  public class OrderTest : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
       DateTime date = new DateTime(2020, 7, 24);
       Order order = new Order("bread", "multigrain", 3, date);
       Assert.AreEqual(typeof(Order), order.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      DateTime date = new DateTime(2020, 7, 24);
+      Order order1 = new Order("bread", "multigrain", 3, date);
+      Order order2 = new Order("bread", "white", 2, date);
+      List<Order> newList = new List<Order> {order1, order2};
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+    
+    [TestMethod]
+    public void GetId_OrderInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      DateTime date = new DateTime(2020, 7, 24);
+      Order order = new Order("bread", "multigrain", 3, date);
+      int result = order.Id;
+      Assert.AreEqual(1, result);
     }
   }
 }
