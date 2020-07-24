1300bac88cc9791f74924ecc4d6e5574e9b24f39
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-24 11:02:09 -0700

    GetAll test passed

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,28 @@ using System;
 namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTest
+  public class VendorTest : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void VendorConstructor_CreatesInstancesOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("Some vendor", "Vendor description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      Vendor vendor1 = new Vendor("Vendor1", "Vendor1 description");
+      Vendor vendor2 = new Vendor("Vendor2", "Vendor2 description");
+      List<Vendor> newList = new List<Vendor> {vendor1, vendor2};
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
