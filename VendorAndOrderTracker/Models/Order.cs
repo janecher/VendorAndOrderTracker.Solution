587e3e96701e488ba988d202bbc49224b8e98346
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-24 10:19:18 -0700

    add properties to Order class, create constructor for passing the test

---
+++ VendorAndOrderTracker/Models/Order.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace VendorAndOrderTracker.Models
+{
+  public class Order
+  {
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Price {get; set;}
+    public DateTime Date {get; set;}
+    public int Id {get;}
+    private static List<Order> _orders = new List<Order>();
+
+    public Order(string title, string description, int price, DateTime date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _orders.Add(this);
+      Id = _orders.Count;
+    }
+  }
+}
